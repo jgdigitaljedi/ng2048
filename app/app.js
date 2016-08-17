4bb2972d0e2d1b4eaf81a5d89ab62719c6b96639
Joey Gauthier <Paul.Gauthier@tolling.telvent.com>
2016-08-17 07:33:03 -0500

    initial commit after basic scaffold setup

---
+++ app/app.js
@@ -0,0 +1,26 @@
+(function() {
+	'use strict';
+
+	/**
+	* @ngdoc index
+	* @name app
+	* @description
+	* # app
+	*
+	* Main module of the application.
+	*/
+
+	angular.module('ng2048', [
+		'ngResource',
+		'ngAria',
+		 'ngMaterial',
+		'ngMdIcons',
+		'ngMessages',
+		'ngCookies',
+		'ngAnimate',
+		'ngSanitize',
+		'ui.router',
+		'home',
+	]);
+
+})();
