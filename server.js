f0e684388ae6b6639fe7c5dd5a4fcf301c94fcbc
Joey Gauthier <Paul.Gauthier@tolling.telvent.com>
2016-08-24 08:06:30 -0500

    super quick mongoose setup, gonna wire it into the app

--- server.js
+++ server.js
@@ -2,12 +2,31 @@
 
 var express  = require('express');
 var app      = express();
-var mongoose = require('mongoose');
 var path = require('path');
 
+// serve files because I didn't use a bloated generator and don't have time to do this a different way
 app.use('/app', express.static(path.join(__dirname, '/app')));
 app.use('/src', express.static(path.join(__dirname, '/src')));
 
+// mongo stuff
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/2048');
+var db = mongoose.connection;
+db.on('error', console.error);
+
+var Scores = require('./schemas/scores.js');
+
+// read and write db
+app.get('/gethighscore', function () {
+
+});
+
+app.post('/newhighscore', function (score) {
+
+});
+
+
+// serve the app
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
